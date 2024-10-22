e28e04273c9b4d80e77e6ee48ff9b86d2b35a3b5
Timchuck <tymoteuszbortnowski@gmail.com>
2024-10-22 18:30:03 +0200

    zkver1

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,72 @@
-const aboutus = document.querySelector('.aboutus');
-const cards = document.querySelectorAll('.card');
-const footerYear = document.querySelector('.footer__year');
-const faq = document.querySelector('.faq__boxes');
-const faqBtns = document.querySelectorAll('.faq__btn');
+document.addEventListener('DOMContentLoaded', () => {
+	const cards = document.querySelectorAll('.card');
+	const footerYear = document.querySelector('.footer__year');
+	const faq = document.querySelector('.faq__boxes');
+	const faqBtns = document.querySelectorAll('.faq__btn');
+	const navMobile = document.querySelector('.nav-mobile');
+	const navDesktop = document.querySelector('.nav-desktop');
+	const navBtn = document.querySelector('.hamburger');
+	const navLink = document.querySelectorAll('.nav-mobile .nav__link');
+	let lastScrollY = window.scrollY;
 
-function showCard() {
-	cards.forEach((card) => {
-		card.classList.remove('active');
-		this.classList.add('active');
+	function showCard() {
+		cards.forEach((card) => {
+			card.classList.remove('active');
+			this.classList.add('active');
+		});
+
+		handleBgColor(this);
+	}
+
+	const handleBgColor = (card) => {
+		const aboutCards = card.getAttribute('data-card');
+		body.setAttribute('data-bg', aboutCards);
+	};
+	function openFaqItems() {
+		if (this.nextElementSibling.classList.contains('active')) {
+			this.nextElementSibling.classList.remove('active');
+		} else {
+			closeFaqItems();
+			this.nextElementSibling.classList.toggle('active');
+		}
+	}
+	const closeFaqItems = () => {
+		const allActiveItems = document.querySelectorAll('.faq__info');
+		allActiveItems.forEach((item) => item.classList.remove('active'));
+	};
+	const clickOutsideFaq = (e) => {
+		if (
+			e.target.classList.contains('faq__btn') ||
+			e.target.classList.contains('faq__info') ||
+			e.target.classList.contains('faq__info-text')
+		)
+			return;
+
+		closeFaqItems();
+	};
+	const handleCurrentYear = () => {
+		const year = new Date().getFullYear();
+		footerYear.innerText = year;
+	};
+
+	const handleNav = () => {
+		navBtn.classList.toggle('is-active');
+		navMobile.classList.toggle('nav-mobile--active');
+	};
+
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > lastScrollY) {
+			navDesktop.classList.add('hidden');
+		} else {
+			navDesktop.classList.remove('hidden');
+		}
+		lastScrollY = window.scrollY;
 	});
 
-	handleBgColor(this);
-}
-
-const handleBgColor = (card) => {
-	const aboutCards = card.getAttribute('data-card');
-	body.setAttribute('data-bg', aboutCards);
-};
-function openFaqItems() {
-	if (this.nextElementSibling.classList.contains('active')) {
-		this.nextElementSibling.classList.remove('active');
-	} else {
-		closeFaqItems();
-		this.nextElementSibling.classList.toggle('active');
-	}
-}
-const closeFaqItems = () => {
-	const allActiveItems = document.querySelectorAll('.faq__info');
-	allActiveItems.forEach((item) => item.classList.remove('active'));
-};
-const clickOutsideFaq = (e) => {
-	if (
-		e.target.classList.contains('faq__btn') ||
-		e.target.classList.contains('faq__info') ||
-		e.target.classList.contains('faq__info-text')
-	)
-		return;
-
-	closeFaqItems();
-};
-const handleCurrentYear = () => {
-	const year = new Date().getFullYear();
-	footerYear.innerText = year;
-};
-
-handleCurrentYear();
-window.addEventListener('click', clickOutsideFaq);
-faqBtns.forEach((btn) => btn.addEventListener('click', openFaqItems));
-cards.forEach((card) => card.addEventListener('click', showCard));
+	handleCurrentYear();
+	window.addEventListener('click', clickOutsideFaq);
+	faqBtns.forEach((btn) => btn.addEventListener('click', openFaqItems));
+	cards.forEach((card) => card.addEventListener('click', showCard));
+	navBtn.addEventListener('click', handleNav);
+	navLink.forEach((link) => link.addEventListener('click', handleNav));
+});
